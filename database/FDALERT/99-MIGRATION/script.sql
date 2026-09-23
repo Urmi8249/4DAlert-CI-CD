e528b5e7da1314da0d17c85cfef68dc0af53a2c3
soumyashree@performalytic.com <soumyashree@performalytic.com>
2026-09-23 15:27:37 +0530

    Deployment Script changes by FDALERT user

--- database/FDALERT/99-MIGRATION/script.sql
+++ database/FDALERT/99-MIGRATION/script.sql
@@ -1,9 +1,15 @@
+create or replace TABLE SOUMYA.EMPLOYEE_TEST (
+	ID NUMBER(38,0),
+	EMP_NAME VARCHAR(100),
+	EMAIL VARCHAR(150),
+	AGE NUMBER(3,0),
+	SALARY NUMBER(10,2),
+	DEPARTMENT VARCHAR(50),
+	IS_ACTIVE BOOLEAN,
+	JOIN_DATE DATE,
+	LAST_UPDATED TIMESTAMP_NTZ(9)
+);
+-----------------------------------------------------------------------------
 
 -----------------------------------------------------------------------------
-create or replace TABLE SOUMYA.ORDERS (
-	ORDER_ID NUMBER(38,0),
-	CUSTOMER_ID NUMBER(38,0),
-	PRODUCT_ID NUMBER(38,0),
-	ORDER_DATE DATE,
-	CUSTOMER_NAME VARCHAR(20)
-);
+DROP TABLE SOUMYA.ORDERS;
