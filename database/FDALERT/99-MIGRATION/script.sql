cba7bfd03a7a3a6b519cf3ba5c14238e0e02616b
soumyashree@performalytic.com <soumyashree@performalytic.com>
2026-09-23 15:13:52 +0530

    Deployment Script changes by FDALERT user

--- database/FDALERT/99-MIGRATION/script.sql
+++ database/FDALERT/99-MIGRATION/script.sql
@@ -1,2 +1,6 @@
-
+ALTER TABLE SOUMYA.ORDERS DROP COLUMN ORD_ID;
+-----------------------------------------------------------------------------
+ALTER TABLE SOUMYA.ORDERS ADD ORDER_ID NUMBER(38,0);
+-----------------------------------------------------------------------------
+ALTER TABLE SOUMYA.ORDERS ADD CUSTOMER_NAME VARCHAR(20);
 -----------------------------------------------------------------------------
