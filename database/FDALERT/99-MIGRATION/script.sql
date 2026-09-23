596fcbe95f6136124d09aef163ab0c31992a9782
soumyashree@performalytic.com <soumyashree@performalytic.com>
2026-09-23 15:00:38 +0530

    Deployment Script changes by FDALERT user

--- database/FDALERT/99-MIGRATION/script.sql
+++ database/FDALERT/99-MIGRATION/script.sql
@@ -1,16 +1,3 @@
-create or replace TABLE SOUMYA.PRODUCT_NEW (
-	PRODUCT_ID NUMBER(38,0),
-	PRODUCT_NAME VARCHAR(100),
-	PRICE NUMBER(10,2)
-);
------------------------------------------------------------------------------
-create or replace TABLE SOUMYA.ORDERS_NEW_2 (
-	ORDER_ID NUMBER(38,0),
-	CUSTOMER_ID NUMBER(38,0),
-	PRODUCT_ID NUMBER(38,0),
-	ORDER_DATE DATE
-);
------------------------------------------------------------------------------
 ALTER TABLE SOUMYA.CUSTOMER_TEST ADD EMAIL VARCHAR(100);
 -----------------------------------------------------------------------------
 create or replace TABLE SOUMYA.EMPLOYEE_TEST (
@@ -25,10 +12,23 @@ create or replace TABLE SOUMYA.EMPLOYEE_TEST (
 	LAST_UPDATED TIMESTAMP_NTZ(9)
 );
 -----------------------------------------------------------------------------
+create or replace TABLE SOUMYA.PRODUCT_NEW (
+	PRODUCT_ID NUMBER(38,0),
+	PRODUCT_NAME VARCHAR(100),
+	PRICE NUMBER(10,2)
+);
+-----------------------------------------------------------------------------
 create or replace TABLE SOUMYA.CUSTOMER_TEST_NEW (
 	CUSTOMER_ID NUMBER(38,0) NOT NULL,
 	CUSTOMER_NAME VARCHAR(100),
 	EMAIL VARCHAR(150),
 	PHONE_NUMBER VARCHAR(20),
 	primary key (CUSTOMER_ID)
+);
+-----------------------------------------------------------------------------
+create or replace TABLE SOUMYA.ORDERS_NEW_2 (
+	ORDER_ID NUMBER(38,0),
+	CUSTOMER_ID NUMBER(38,0),
+	PRODUCT_ID NUMBER(38,0),
+	ORDER_DATE DATE
 );
