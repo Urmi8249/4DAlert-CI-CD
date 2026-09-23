2baeaea3c41a766f20f96d2bc759b3f9ebd57e06
Urmila Sahoo <112173512+Urmi8249@users.noreply.github.com>
2026-09-23 11:40:46 +0530

    Revert "[+2 ~1 -1]  → Basanta
    Update: TABLE — DUMMY.ORDER_ITEMS
    Remove: TABLE — DUMMY.EMPLOYEEE.sql
    Add: VIEW — DUMMY.EMPLOYEE_STATUS_VIEW; STORED_PROCEDURE — DUMMY.GET_EMPLOYEE_STATUS"

--- SNOWCICD/DUMMY/VIEW/EMPLOYEE_STATUS_VIEW.sql
+++
@@ -1,9 +0,0 @@
-create or replace view DUMMY.EMPLOYEE_STATUS_VIEW(
-	STATUS,
-	SALARY,
-	SALARY_LEVEL
-) as
-SELECT
-    'Employee is Active' AS STATUS,
-    50000 AS SALARY,
-    'HIGH' AS SALARY_LEVEL;
